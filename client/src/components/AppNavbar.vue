86f95d0b716640e669ffcd6bb0fcfda015570ae1
Aleksander Dimitrijevic <aleksander88.dev@gmail.com>
2020-09-25 18:40:05 +0200

    attached new styles to app bar

--- client/src/components/AppNavbar.vue
+++ client/src/components/AppNavbar.vue
@@ -1,10 +1,18 @@
 <template>
   <nav class="app-bar">
-    <button v-if="!$store.getters.loggedIn" class="btn-user-actions btn-login" @click="loginUser">
+    <button
+      v-if="!$store.getters.loggedIn"
+      class="btn-user-actions btn-login"
+      @click="loginUser"
+    >
       <span class="lnr lnr-user"></span> Login
     </button>
-    <button v-else class="btn-user-actions" @click="logout">Logout</button>
-    <button v-show="$store.getters.loggedIn">Create new event</button>
+    <div class="btn-content" v-else>
+      <button class="btn-user-actions" @click="logout">Logout</button>
+      <button v-show="$store.getters.loggedIn" class="btn-user-actions">
+        Create event
+      </button>
+    </div>
   </nav>
 </template>
 
@@ -32,20 +40,27 @@ export default {
   width: 100%;
   height: 60px;
   display: flex;
+  position: fixed;
   padding: 0 1rem;
-  position: absolute;
   align-items: center;
+  background: rgba(#000, 0.1);
+  .btn-content {
+    margin-left: auto;
+  }
 
   .btn-user-actions {
     border: none;
-    color: #fff;
+    color: #000;
     transition: 0.3s;
     font-size: 1.2rem;
-    margin-left: auto;
     border-radius: 3px;
+    margin-left: 0.5rem;
     padding: 0.5rem 1rem;
-    background: rgba(#000, 0.4);
+    background: rgba(#fff, 0.9);
 
+    &.btn-login {
+      margin-left: auto;
+    }
     span {
       transition: 0.3s;
     }
@@ -59,9 +74,6 @@ export default {
     .btn-user-actions {
       &:hover {
         opacity: 0.8;
-        span {
-          color: red;
-        }
       }
     }
   }
