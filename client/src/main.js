1814d77e787700cd10f4cf5e031d1da9694783eb
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-09-28 13:09:56 +0200

    made vuex changes

--- client/src/main.js
+++ client/src/main.js
@@ -15,15 +15,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  created() {
-    const events = localStorage.getItem("events");
-    const reviews = localStorage.getItem("reviews");
-    const isLoggedIn = localStorage.getItem("user");
-    if (isLoggedIn) {
-      store.commit("SET_EVENTS", JSON.parse(events));
-      store.commit("SET_REVIEWS", JSON.parse(reviews));
-      store.commit("SET_USER_DATA", JSON.parse(isLoggedIn));
-    }
-  },
   render: (h) => h(App),
 }).$mount("#app");
--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -13,26 +13,15 @@ export const mutations = {
   SET_LOADING(state) {
     state.isLoading = !state.isLoading;
   },
-  SET_USER_DATA(state, userData) {
-    state.user = userData;
-  },
   SET_EVENT_DATA(state, event) {
     const eventExists = state.eventList.some((item) => item.id === event.id);
     if (eventExists) {
       state.eventList = state.eventList.filter((e) => e.id !== event.id);
     } else {
       state.eventList.push(event);
-      localStorage.setItem("events", JSON.stringify(state.eventList));
     }
   },
   SET_EVENT_REVIEW(state, eventReview) {
     state.eventReviews.push(eventReview);
-    localStorage.setItem("reviews", JSON.stringify(state.eventReviews));
-  },
-  SET_REVIEWS(state, reviews) {
-    state.eventReviews = reviews;
-  },
-  SET_EVENTS(state, events) {
-    state.eventList = events;
   },
 };
