1a6ec8bd39a4cca680dbaa079d56110c1e8c7b90
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-09-30 12:45:32 +0200

    push to heroku

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 const cors = require("cors");
-
 const fakeRouter = require("./routes/events");
 
 app.use(express.json());
@@ -13,16 +12,9 @@ app.use("/api/events", fakeRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/public")));
-  app.get("/", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "/public", "index.html"))
   );
 }
-
-app.get("/", (req, res) => {
-  app.use(express.static(path.join(__dirname, "../client/public")));
-  app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/public", "index.html"))
-  );
-});
 
 app.listen(PORT, () => console.log(`Server up and running on port: ${PORT}`));
